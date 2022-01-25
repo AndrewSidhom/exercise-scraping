f81d196f13c2b8531686f42c0d5a53c7080480ec
Andrew Sidhom <andrew_59@hotmail.com>
2022-01-25 17:08:45 -0500

    First commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import requests
+
+
+def load_input_data():
+    pass
+
+
+def submit_form():
+    pass
+
+
+if __name__ == '__main__':
+    load_input_data()
+    submit_form()
